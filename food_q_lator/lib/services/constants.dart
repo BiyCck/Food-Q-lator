b59f9878040a09ff7736898993604c3e79d5c193
BiyCck <98288128+BiyCck@users.noreply.github.com>
2022-11-17 15:27:16 +0100

    Added color palette

--- food_q_lator/lib/services/constants.dart
+++ food_q_lator/lib/services/constants.dart
@@ -5,3 +5,21 @@ const kTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   color: Colors.white,
 );
+
+//Primaries
+const kPrimaryColor = Color(0xFF00BCD4);
+
+const kDarkPrimaryColor = Color(0xFF0097A7);
+
+const kLightPrimaryColor = Color(0xFFB2EBF2);
+
+const kTextFieldAndIconColor = Colors.white;
+
+//Secondaries
+const kPrimaryTextColor = Colors.black;
+
+const kSecondaryText = Color(0xFF757575);
+
+const kDividerColor = Color(0xFFBDBDBD);
+
+const kAccentColor = Color(0xFFFF5722);

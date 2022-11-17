a67b8a4910e134edc6df85ca9e3391361d4b65f6
Börge Meyer <boergermeisterr@icloud.com>
2022-11-17 14:29:09 +0100

    skip - text

--- food_q_lator/lib/screens/onboarding2.dart
+++ food_q_lator/lib/screens/onboarding2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_q_lator/services/constants.dart';
 
 class Onboarding2 extends StatefulWidget {
   const Onboarding2({super.key});
@@ -12,8 +13,42 @@ class Onboarding2 extends StatefulWidget {
 class _Onboarding2State extends State<Onboarding2> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text("optimaler_Huso"),Text("Huso")],
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(0.0),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Color.fromARGB(255, 255, 255, 255),
+              height: 100,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: const Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Text(
+                          'Skip >',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 0, 191, 255),
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          )
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ), 
+            )
+          ],
+        ),
+      ),
     );
   }
 }

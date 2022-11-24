6ed75d1ad9e2a3895f744830cfe08ae4818b3110
Börge Meyer <boergermeisterr@icloud.com>
2022-11-24 11:22:44 +0100

    Onboarding 2 Seite : Pfeil, Dynamik, Bild und Menüleiste fehlen

--- food_q_lator/lib/screens/onboarding2.dart
+++ food_q_lator/lib/screens/onboarding2.dart
@@ -17,35 +17,96 @@ class _Onboarding2State extends State<Onboarding2> {
       color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.all(0.0),
-          child: Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              color: Color.fromARGB(255, 255, 255, 255),
+                height: 100,
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      TextButton(
+                        child: const Text('SKIP >',
+                            style: TextStyle(
+                              color: Color.fromRGBO(0, 188, 212, 1),
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                            )),
+                        onPressed: null,
+                      )
+                    ],
+                  ),
+                )),
+            Container(
+              color: Colors.red,
+              height: 550,
+            ), //Container - Picture
+            Container(
+                height: 150,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.white,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: Padding(
+                          padding: EdgeInsets.all(0),
+                          child: Text(
+                            'GPS Tracking',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 45, 45, 45),
+                              decoration: TextDecoration.none,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24,
+                            ),
+                          )),
+                    ),
+                    Container(
+                      height: 100,
+                      alignment: Alignment.center,
+                      child: Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Text(
+                            'Loved the class! Such beautiful land and collective impact infrastructure social entrepreneur.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 110, 110, 110),
+                              decoration: TextDecoration.none,
+                              fontWeight: FontWeight.w300,
+                              fontSize: 18,
+                            ),
+                          )),
+                    )
+                  ],
+                )), //Container - Text
+            Container(
               height: 100,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Expanded(
-                    child: const Align(
-                      alignment: Alignment.bottomRight,
-                      child: Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          'Skip >',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 0, 191, 255),
-                            decoration: TextDecoration.none,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18,
-                          )
-                        ),
-                      ),
-                    ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        padding: EdgeInsets.only(
+                            top: 15, right: 120, bottom: 15, left: 120),
+                        backgroundColor: Color.fromRGBO(0, 188, 212, 1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15)))),
+                    child: const Text('CONTINUE',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        )),
+                    onPressed: null,
                   )
                 ],
-              ), 
-            )
+              ),
+            ) //Container - Button
           ],
         ),
       ),

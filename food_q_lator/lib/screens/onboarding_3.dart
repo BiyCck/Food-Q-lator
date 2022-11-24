43ecf157fabb4f3bf06f5b7db5ca4ecc5f9f9a9f
Victor <victorr.dickert@myhsba.de>
2022-11-24 11:08:27 +0100

    Seite3start

--- food_q_lator/lib/screens/onboarding_3.dart
+++ food_q_lator/lib/screens/onboarding_3.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_q_lator/screens/home_screen.dart';
+import 'package:food_q_lator/services/constants.dart';
 
 class Onboarding3 extends StatefulWidget {
   const Onboarding3({super.key});
@@ -20,21 +22,43 @@ class _Onboarding3State extends State<Onboarding3> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           
           children: [
-            Text("Im Optimalfall bleibt keiner liegen", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0,),),
+            Text("Jetzt starten!", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0, fontFamily: 'Montsserrat'),),
             SizedBox(height: 20.0,),
-            Text("Leben auf dem Drahtseil, das 187", textAlign: TextAlign.center),
+            Text("Anmelden oder direkt loslegen", textAlign: TextAlign.center),
             SizedBox(height: 40.0,),
             TextButton(
                   style: TextButton.styleFrom(
-                    foregroundColor: Colors.green,
-                    padding: const EdgeInsets.all(16.0),
+                    foregroundColor: Colors.white,
+                    backgroundColor: kAccentColor,
+                    padding: const EdgeInsets.all(15.0),
                     textStyle: const TextStyle(fontSize: 20),
-                    
                   ),
                   onPressed: () {},
-                  child: const Text('187'),
+                  child: const Text('Mit Google anmelden'),
                 ),
-          
+            SizedBox(height: 20.0,),
+            TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: kPrimaryColor,
+                    padding: const EdgeInsets.all(15.0),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Anmelden'),
+                ),
+            SizedBox(height: 20.0,),
+            TextButton(
+              style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: kLightPrimaryColor,
+                    padding: const EdgeInsets.all(15.0),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+              onPressed: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const HomeScreen()),
+                ),
+                 child: const Text('Überspringen')),
           ],
         ),
       ),

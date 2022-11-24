7e55be7cb8386aac22864b0da5c1ae25a1a1a11c
BiyCck <98288128+BiyCck@users.noreply.github.com>
2022-11-24 10:16:02 +0100

    Added SafeArea

--- food_q_lator/lib/screens/onboarding1.dart
+++ food_q_lator/lib/screens/onboarding1.dart
@@ -15,82 +15,106 @@ class Onboarding1 extends StatefulWidget {
 }
 
 class _Onboarding1State extends State<Onboarding1> {
-  int counter=0;
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              //TODO: richtigen Link einfügen
-              DefaultButton(color: Color(33), text: 'Skip', onPressed: () {
-                setState(() {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()),);
-                },
-                );
-              }
-              ),
-            ],
-          ),
-          Expanded (child:Column(
-            children: [
-              //Picture
-              Expanded(child: Column (
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(child: Container(
-                      decoration: BoxDecoration(
-                      image: DecorationImage(image: AssetImage(
-                          'images/ikigai-udon-recipe-1.png'), fit: BoxFit.fill
-                    ),
-                  )
-                  ),
-                  ),
-                ],
-              ),
-              ),
-              //Text unter dem Bild
-              Expanded(child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(child: Column(
-                    children: [
-                      //TODO: Überschrift schreiben
-                      Text('Hier könnte Ihre Überschrift stehen!', style: kTextStyle,),
-                      SizedBox(height: 30,),
-                      //TODO: Text unter Überschrift schreiben
-                      Text('Text unter Überschrift')
-                    ],
-                  ),
-                  ),
-                  Expanded(child: Column(
-                    children: [
-                      //TODO: Weiterleitung auf onboarding2
-                      DefaultButton(color: Color(33), text: 'Weiter', onPressed: () {
-                        setState(() {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()),);
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                //TODO: richtigen Link einfügen
+                DefaultButton(
+                    color: Color(33),
+                    text: 'Skip',
+                    onPressed: () {
+                      setState(
+                        () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeScreen()),
+                          );
                         },
-                        );
-                      },
-                      ),
-                    ],
+                      );
+                    }),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  //Picture
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                              decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    'images/ikigai-udon-recipe-1.png'),
+                                fit: BoxFit.fill),
+                          )),
+                        ),
+                      ],
+                    ),
                   ),
+                  //Text unter dem Bild
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              //TODO: Überschrift schreiben
+                              Text(
+                                'Hier könnte Ihre Überschrift stehen!',
+                                style: kTextStyle,
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              //TODO: Text unter Überschrift schreiben
+                              Text('Text unter Überschrift')
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              //TODO: Weiterleitung auf onboarding2
+                              DefaultButton(
+                                color: Color(33),
+                                text: 'Weiter',
+                                onPressed: () {
+                                  setState(
+                                    () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const HomeScreen()),
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-              ),
-
-
-            ],
-          ),
-
-
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }

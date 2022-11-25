7e5f6b1b83919b5d7d118f49353735e8fc7cbfea
BiyCck <98288128+BiyCck@users.noreply.github.com>
2022-11-25 13:15:51 +0100

    Added loadingAnimation

--- food_q_lator/lib/screens/home_screen.dart
+++ food_q_lator/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,27 +14,43 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    startAnimation();
+  }
+
+  startAnimation() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushNamed(context, '/onboarding1');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF00BCD4),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Welcome to the Food-Q-Lator',
-                style: kTextStyle,
-                textAlign: TextAlign.center,
-              ),
+      backgroundColor: const Color(0xFF00BCD4),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Welcome to the Food-Q-Lator',
+              style: kTextStyle,
+              textAlign: TextAlign.center,
             ),
-            Icon(
-              Icons.calculate,
-              color: Colors.white,
-              size: 150.0,
-            ),
-          ],
-        ));
+          ),
+          const Icon(
+            Icons.calculate,
+            color: Colors.white,
+            size: 150.0,
+          ),
+        ],
+      ),
+    );
   }
 }
